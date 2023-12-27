68e57116a79a16237a5f3db0a193693c98d9f357
Apoorv Sharma <apoorv.sh98@gmail.com>
2023-12-27 15:34:51 -0500

    Adding wire-frame | Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+import Home from "../src/components/Home"
+import NavBar from "../src/components/NavBar"
+import Projects from "../src/components/Projects"
+import WorkEx from "../src/components/WorkEx"
+import Blog from "../src/components/Blog"
+import BucketList from "../src/components/BucketList"
+import Resume from "../src/components/Resume"
+import Footer from "../src/components/Footer"
+import ScrollToTop from "./components/ScrollToTop";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import Typewriter from "typewriter-effect";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ScrollToTop />
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/workEx" element={<WorkEx />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/bucketList2024" element={<BucketList />} />
+          <Route path="/Resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
